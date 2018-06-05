776d7898b29c79f7c9b7e8b5e40cdbf31a8d7309
katrina <katrina.schwark@gmail.com>
2018-06-05 14:31:20 -0600

    Initial push
    Initial file push

---
+++ handler.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['firstname','lastname', 'email','phone'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+$pp->attachFiles(['image']);
+
+$pp->requireReCaptcha();
+$pp->getReCaptcha()->initSecretKey('6LdHMF0UAAAAAJOWbnIEdIpJt01jNSZcvzl5yl7U');
+
+
+$pp->sendEmailTo('k.williams80@gmail.com'); // ← Your email here
+
+$mailer = $pp->getMailer();
+$mailer->setFrom('notice@etucker.net','Form Submission from eTucker.net',false);
+
+
+
+echo $pp->process($_POST);
